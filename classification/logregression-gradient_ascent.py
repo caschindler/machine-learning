3ff03575796ad528c4a6c842a211f1bca67ed1a2
Carsten Schindler <cmi.schindler@gmail.com>
2016-06-10 15:31:08 +0200

    Adding methods

---
+++ classification/logregression-gradient_ascent.py
@@ -0,0 +1,155 @@
+﻿# LOAD GRAPHLAB TO BE ABLE TO USE SFRAMES/SARRAYS
+
+import graphlab
+
+# EXAMPLE RUNS ON AN AMAZON PRODUCT REVIEW DATASET (BABY PRODUCTS)
+
+products = graphlab.SFrame('amazon_baby_subset.gl/')
+
+
+# CHECK BALANCE OF POSITIVE AND NEGATIVE REVIEWS
+
+print '# of positive reviews =', len(products[products['sentiment']==1])
+print '# of negative reviews =', len(products[products['sentiment']==-1])
+
+
+# APPLY TEXT CLEANING ON DATA
+
+# Perform some simple feature cleaning using SFrames. 
+# We will not use bag-of-words features, but limit ourselves to 193 words (for simplicity). 
+# We compiled a list of 193 most frequent words into a JSON file and will load these words from this JSON file.
+
+import json
+with open('important_words.json', 'r') as f: # Reads the list of most frequent words
+    important_words = json.load(f)
+important_words = [str(s) for s in important_words]
+
+# Now, we will perform 2 simple data transformations:
+# 1. Remove punctuation using Python's built-in string functionality.
+
+def remove_punctuation(text):
+    import string
+    return text.translate(None, string.punctuation) 
+
+products['review_clean'] = products['review'].apply(remove_punctuation)
+
+# 2. Compute word counts (only for important_words)
+
+for word in important_words:
+    products[word] = products['review_clean'].apply(lambda s : s.split().count(word))
+
+# Example: Compute the number of product reviews that contain the word perfect.
+contains_perfect = products['perfect'] >= +1
+print contains_perfect.sum()
+
+
+# CONVERT SFRAME TO NUMPY ARRAY
+
+import numpy as np
+
+def get_numpy_data(data_sframe, features, label):
+    data_sframe['intercept'] = 1
+    features = ['intercept'] + features
+    features_sframe = data_sframe[features]
+    feature_matrix = features_sframe.to_numpy()
+    label_sarray = data_sframe[label]
+    label_array = label_sarray.to_numpy()
+    return(feature_matrix, label_array)
+
+feature_matrix, sentiment = get_numpy_data(products, important_words, 'sentiment')
+feature_matrix.shape
+
+
+# ESTIMATE CONDITIIONAL PROBABILITY WITH LINK FUNCTION
+# Produces probablistic estimate for P(y_i = +1 | x_i, w) and estimate ranges between 0 and 1.
+
+def predict_probability(feature_matrix, coefficients): 
+    scores = np.dot(feature_matrix, coefficients)
+    predictions = 1 / (1 + np.exp(-scores))
+    return predictions
+
+
+# COMPUTE THE LOG LIKELIHOOD FOR THE ENTIRE DATASET
+
+def compute_log_likelihood(feature_matrix, sentiment, coefficients):
+    indicator = (sentiment==+1)
+    scores = np.dot(feature_matrix, coefficients)
+    logexp = np.log(1. + np.exp(-scores))
+    
+    # Simple check to prevent overflow
+    mask = np.isinf(logexp)
+    logexp[mask] = -scores[mask]
+    
+    lp = np.sum((indicator-1)*scores - logexp)
+    return lp
+
+
+# IMPLEMENT LOGISTIC REGRESSION WITH GRADIENT ASCENT
+
+from math import sqrt
+
+def logistic_regression(feature_matrix, sentiment, initial_coefficients, step_size, max_iter):
+    coefficients = np.array(initial_coefficients) # make sure it's a numpy array
+    for itr in xrange(max_iter):
+
+        # Compute the errors between indicator value for (y_i = +1) and prediction P(y_i = +1|x_i,w)
+        scores = np.dot(feature_matrix, coefficients)
+        predictions = 1 / (1 + np.exp(-scores))
+        indicator = (sentiment==+1)
+        errors = indicator - predictions    
+
+  	# loop over each coefficient 
+        for j in xrange(len(coefficients)): 
+      
+            # Compute the derivative for coefficient[j].
+            derivative = np.dot(errors, feature_matrix[:,j])  
+         
+            # add the step size times the derivative to the current coefficient
+            coefficients[j] = coefficients[j] + step_size * derivative   
+     
+        # Check whether log likelihood is increasing
+        if itr <= 15 or (itr <= 100 and itr % 10 == 0) or (itr <= 1000 and itr % 100 == 0) \
+        or (itr <= 10000 and itr % 1000 == 0) or itr % 10000 == 0:
+            lp = compute_log_likelihood(feature_matrix, sentiment, coefficients)
+            print 'iteration %*d: log likelihood of observed labels = %.8f' % \
+                (int(np.ceil(np.log10(max_iter))), itr, lp)
+    return coefficients
+
+coefficients = logistic_regression(feature_matrix, sentiment, initial_coefficients=np.zeros(194), step_size=1e-7, max_iter=301)
+
+
+# PREDICT SENTIMENT AND MEASURE ACCURACY
+
+scores = np.dot(feature_matrix, coefficients)
+class_prediction = scores / np.abs(scores)
+print (class_prediction == 1).sum()
+print (class_prediction == -1).sum()
+print len(class_prediction)
+
+# Measure accuracy
+
+num_mistakes = (sentiment != class_prediction).sum()
+num_correct = len(products) - num_mistakes
+accuracy = float(num_correct) / len(products)
+print "-----------------------------------------------------"
+print '# Reviews   correctly classified =', len(products) - num_mistakes
+print '# Reviews incorrectly classified =', num_mistakes
+print '# Reviews total                  =', len(products)
+print "-----------------------------------------------------"
+print 'Accuracy = %.2f' % accuracy
+
+
+# IDENTIFYING WORDS THAT CONTRIBUTE MOST TO POSITIVE AND NEGATIVE SENTIMENT
+# Treat each coefficient as a tuple, i.e. (word, coefficient_value).
+# Sort all the (word, coefficient_value) tuples by coefficient_value in descending order.
+
+coefficients = list(coefficients[1:]) # exclude intercept
+word_coefficient_tuples = [(word, coefficient) for word, coefficient in zip(important_words, coefficients)]
+word_coefficient_tuples = sorted(word_coefficient_tuples, key=lambda x:x[1], reverse=True)
+
+# 10 most positive
+print word_coefficient_tuples[:10]
+
+# 10 most negative (two lines due to Python indexing)
+print word_coefficient_tuples[-10:-1]
+print word_coefficient_tuples[-1]
---
+++ regression/knearest-model_and_kchoice.py
@@ -0,0 +1,133 @@
+﻿# EXAMPLE APPLIED TO A HOUSING DATASET
+
+import graphlab
+import numpy as np
+
+
+# RANDOM SELECTION OF 40% OF THE FULL DATASET
+
+sales = graphlab.SFrame('kc_house_data_small.gl/')
+
+
+# DEFINE FUNCTIONS FOR DATA EXTRACTION AND NORMALIZATION
+
+def get_numpy_data(data_sframe, features, output):
+    data_sframe['constant'] = 1
+    features = ['constant'] + features
+    features_sframe = data_sframe[features]
+    feature_matrix = features_sframe.to_numpy()
+    output_sarray = data_sframe[output]
+    output_array = output_sarray.to_numpy()
+    return(feature_matrix, output_array)
+
+def normalize_features(feature_matrix):
+    norms = np.linalg.norm(feature_matrix, axis=0)
+    normalized_features = feature_matrix / norms
+    return (normalized_features, norms)
+
+
+# SPLIT DATA INTO TRAINING, VALIDATION AND TEST SET
+
+(train_and_validation, test) = sales.random_split(.8, seed=1)
+(train, validation) = train_and_validation.random_split(.8, seed=1)
+
+
+# EXTRACT FEATURES AND NORMALIZE
+
+feature_list = ['bedrooms',  
+                'bathrooms',  
+                'sqft_living',  
+                'sqft_lot',  
+                'floors',
+                'waterfront',  
+                'view',  
+                'condition',  
+                'grade',  
+                'sqft_above',  
+                'sqft_basement',
+                'yr_built',  
+                'yr_renovated',  
+                'lat',  
+                'long',  
+                'sqft_living15',  
+                'sqft_lot15']
+
+features_train, output_train = get_numpy_data(train, feature_list, 'price')
+features_test, output_test = get_numpy_data(test, feature_list, 'price')
+features_valid, output_valid = get_numpy_data(validation, feature_list, 'price')
+
+features_train, norms = normalize_features(features_train)
+features_test = features_test / norms # normalize test set by training set norms
+features_valid = features_valid / norms # normalize validation set by training set norms
+
+
+# FOR FUN: FUNCTION TO CALCULATE DISTANCE FROM ONE QUERY HOUSE TO ALL TRAINING HOUSES.
+
+def euclid_distance(feature_matrix, query_vector):
+    diff = feature_matrix[0:len(feature_matrix)] - query_vector
+    distances = np.sqrt(np.sum(diff**2, axis=1))
+    return distances
+
+# Find closest one
+
+nearest_neighbour_distance = euclid_distance(features_train, features_test[2])
+min_distance_index, min_distance = min(enumerate(nearest_neighbour_distance), key=lambda p: p[1])
+print min_distance_index, min_distance
+
+
+# PERFORM K-NEAREST NEIGHBOR REGRESSION
+
+# FETCH K-NEAREST NEIGHBORS
+
+def k_nearest_neighbors(k, feature_matrix, query_vector):
+    diff = feature_matrix[0:len(feature_matrix)] - query_vector
+    distances = np.sqrt(np.sum(diff**2, axis=1))
+    k_nearest = np.argpartition(distances, k)[:k]
+    return k_nearest
+
+# Note: np.argpartition guarantees that the kth element is in sorted position and all smaller elements will be moved before it. 
+# ...Thus the first k elements will be the k-smallest elements.
+# ...It does not sort the entire array!
+
+k_nearest_neighbors(4, features_train, features_test[2])
+
+
+# MAKE A PREDICTION ON A SINGLE QUERY
+
+def predict_output(k, feature_matrix, output, query_vector):
+    k_nearest = k_nearest_neighbors(k, feature_matrix, query_vector)
+    yhat = output[k_nearest].mean()
+    return yhat
+
+predict_output(4, features_train, output_train, features_test[2])
+
+
+# MAKE PREDICTIONS ON MULTIPLE QUERIES
+
+def predict_output(k, feature_matrix, output, query_feature_matrix):
+    yhat_list = []
+    for i in xrange(len(query_feature_matrix)):
+        k_nearest = k_nearest_neighbors(k, feature_matrix, query_feature_matrix[i])
+        yhat = output[k_nearest].mean()
+        yhat_list.append(yhat)
+    return yhat_list
+
+predict_output(10, features_train, output_train, features_test[0:10])
+
+
+# CHOOSING THE BEST VALUE OF K USING A VALIDATION SET
+
+rss_all = []
+for k in xrange(1,16):
+    yhat = predict_output(k, features_train, output_train, features_valid)
+    rss_all.append(((yhat - output_valid)**2).sum())
+print rss_all
+
+
+# To visualize the performance as a function of k, plot the RSS on the VALIDATION set for each considered k value:
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+kvals = range(1, 16)
+plt.plot(kvals, rss_all,'bo-')
